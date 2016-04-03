87955e66fd7b9cdead7ff2f5d58c8a0210ab2aec
索丘 <zengyue.yezy@alibaba-inc.com>
2016-04-03 23:14:08 +0800

    add mongodb

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var koa = require('koa');
 var logger = require('koa-logger');
 var route = require('./app/route');
 var nunjucks = require('koa-nunjucks-render');
+var mongo = require('koa-mongo');
 
 var app = koa();
 
@@ -12,6 +13,21 @@ app.use(logger());
 
 // nunjucks render
 app.use(nunjucks('./app/views', {}));
+
+app.use(mongo({
+  db: 'test'
+}));
+// app.use(mongo({
+//   host: 'localhost',
+//   port: 27017,
+//   user: 'admin',
+//   pass: '123456',
+//   db: 'test',
+//   max: 100,
+//   min: 1,
+//   timeout: 30000,
+//   log: false
+// }));
 
 // route middleware
 route(app);
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,15 @@
 'use strict';
 
 exports.index = function* () {
-  this.body = yield this.render('home.html', {
-    message: 'Hello World!',
-    engine: 'Thomas The Tank',
-    helicopter: true
+  this.mongo.collection('users').insertOne({users: 'test'}, function (err, doc) {
+    console.log(doc);
   });
+
+  this.body = yield this.mongo.collection('users').findOne();
+
+  // this.body = yield this.render('home.html', {
+  //   message: 'Hello World!',
+  //   engine: 'Thomas The Tank',
+  //   helicopter: true
+  // });
 }
