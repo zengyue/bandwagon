2006fe7639b351ac97e61a538b400cb1dbcf3a64
索丘 <zengyue.yezy@alibaba-inc.com>
2016-04-04 15:38:52 +0800

    update controller

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,15 +1,6 @@
 'use strict';
 
 exports.index = function* () {
-  this.mongo.collection('users').insertOne({users: 'test'}, function (err, doc) {
-    console.log(doc);
-  });
-
-  this.body = yield this.mongo.collection('users').findOne();
-
-  // this.body = yield this.render('home.html', {
-  //   message: 'Hello World!',
-  //   engine: 'Thomas The Tank',
-  //   helicopter: true
-  // });
+  const users = yield this.mongo.collection('users').findOne();
+  yield this.render('home.html', users);
 }
